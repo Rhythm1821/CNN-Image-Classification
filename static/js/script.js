820c66e5ba4f5b5c8a8324344d799a5c65a5c55a
Rhythm1821 <alamrawat1821@gmail.com>
2023-08-03 15:32:24 +0530

    Static folder updated

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,4 @@
+
 function previewImage(event) {
   const image = document.getElementById('imagePreview');
   image.innerHTML = '';
@@ -15,20 +16,34 @@ function previewImage(event) {
   }
 }
 
-function predictImage() {
+async function predictImage() {
   const fileInput = document.getElementById('imageUpload');
   const file = fileInput.files[0];
   const formData = new FormData();
   formData.append('file', file);
 
-  fetch('/predict', {
-    method: 'POST',
-    body: formData,
-  })
-  .then(response => response.json())
-  .then(data => {
-    const resultDiv = document.getElementById('predictionResult');
-    resultDiv.textContent = `Prediction: ${data.prediction}`;
-  })
-  .catch(error => console.error('Error:', error));
+  try {
+    const response = await fetch('/predict', {
+      method: 'POST',
+      body: formData,
+    });
+
+    if (!response.ok) {
+      throw new Error('Prediction request failed. Status: ' + response.status);
+    }
+
+    const data = await response.json();
+
+    const predictionResult = document.getElementById('predictionResult');
+    if (data.prediction === 'happy') {
+      predictionResult.innerHTML = '<div class="alert alert-success prediction-alert" role="alert">Happy 😃</div>';
+    } else if (data.prediction === 'sad') {
+      predictionResult.innerHTML = '<div class="alert alert-danger prediction-alert" role="alert">Sad 😢</div>';
+    } else {
+      predictionResult.innerHTML = '<div class="alert alert-warning prediction-alert" role="alert">Model could not predict the image. Try another one.</div>';
+    }
+  } catch (error) {
+    console.error('Error:', error.message);
+    alert('Prediction failed. Please try again.');
+  }
 }
